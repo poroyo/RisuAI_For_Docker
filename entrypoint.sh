504ec901b3805d4042aca69cae0772d22e98eb4b
whosthiya57 <132068975+whosthiya57@users.noreply.github.com>
2023-08-09 01:22:46 +0900

    Add debugging logs to startup script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+echo "Checking PULL_GIT variable: $PULL_GIT"
+
 if [ "$PULL_GIT" = "true" ]
 then
+  echo "PULL_GIT is true, executing git pull"
   git pull
+else
+  echo "PULL_GIT is not true, skipping git pull"
 fi
+
+echo "Executing server.sh script"
 ./server.sh
